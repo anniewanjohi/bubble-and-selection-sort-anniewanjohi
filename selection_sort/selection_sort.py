85cef1c8afd0aa570ef759d39eeb5aa10cd89797
anniewanjohi <annieww24@gmail.com>
2025-07-07 21:30:21 +0300

    selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n):
+        # Assume the current index is the minimum
+        min_index = i
+        # Find the index of the smallest element in the remaining list
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Swap if a smaller element was found
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
